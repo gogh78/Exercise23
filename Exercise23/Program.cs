6f64ac2ae9565818c68b2a4e6daff902a10d3b03
Алексей <gogh78@mail.ru>
2022-01-05 16:03:03 +0400

    Добавьте файлы проекта.

---
+++ Exercise23/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex23
+{
+    class Program
+    {
+        //Разработать асинхронный метод для вычисления факториала числа.
+        //В методе Main выполнить проверку работы метода.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для вычисления его факториала");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("!{0} = ", n);
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Factorial(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+                Thread.Sleep(50);
+            }
+            Console.WriteLine(s);
+        }
+
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
